a47b5a4af43950092ab2d359c5bc44fc9a0a089b
James <james@ieork1-wifi-17-65-254-39.euro.apple.com>
2020-02-09 22:04:03 +0000

    [fix] removed redundant code

--- timesheet_first/src/app/models/taskState.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum TaskState {
-    NEW = 1,
-    ACTIVE = 2,
-    EDITABLE = 3,
-    SUBMITTED = 4
-}
--- timesheet_first/src/app/models/taskType.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum TaskType {
-    TELEPHONE_CALL = 1,
-    DRAFTING_DOCUMENT = 2,
-    RESEARCH = 3
-}
