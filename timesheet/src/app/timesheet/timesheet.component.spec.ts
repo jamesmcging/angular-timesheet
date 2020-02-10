c9b13069a2407d8d4c6481d3d75fe6d9810c2a6e
James <james@ieork1-wifi-17-65-254-39.euro.apple.com>
2020-02-10 17:13:56 +0000

    [fix] timesheet test
    - managed to set up component test that relies on service

--- timesheet/src/app/timesheet/timesheet.component.spec.ts
+++ timesheet/src/app/timesheet/timesheet.component.spec.ts
@@ -2,33 +2,42 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TimeSheetComponent } from './timesheet.component';
 import { TimeSheetService } from '../timesheet.service';
+import { BillableComponent} from '../billable/billable.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TimeSheet} from '../models/timesheet.class';
+import {Billable} from '../models/billable.class';
 
 describe('TimesheetComponent', () => {
-    // let service: TimeSheetService;
-    // let component: TimeSheetComponent;
-    // let fixture: ComponentFixture<TimeSheetComponent>;
-    //
-    // beforeEach(async(() => {
-    //     TestBed.configureTestingModule({
-    //         declarations: [
-    //             TimeSheetComponent
-    //         ],
-    //         providers: [
-    //             TimeSheetService
-    //         ]
-    //     })
-    //     .compileComponents();
-    // }));
-    //
-    // beforeEach(() => {
-    //     fixture = TestBed.createComponent(TimeSheetComponent);
-    //     component = fixture.componentInstance;
-    //     // Fetch timesheet from timesheet service
-    //     component.objTimeSheet = service.getTimeSheet();
-    //     fixture.detectChanges();
-    // });
-    //
-    // it('should create', () => {
-    //     expect(component).toBeTruthy();
+    let service: TimeSheetService;
+    let component: TimeSheetComponent;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+                declarations: [
+                    TimeSheetComponent,
+                    BillableComponent
+                ],
+                imports: [
+                    FormsModule,
+                    ReactiveFormsModule
+                ],
+                providers: []
+        })
+        .compileComponents();
+    }));
+
+    beforeEach(() => {
+        service = new TimeSheetService();
+        component = new TimeSheetComponent(service);
+        component.objTimeSheet = service.getTimeSheet();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    // it ('should return an array of billables', () => {
+    //     const objBillable = new Billable();
+    //     expect(component.getBillables()).toEqual(jasmine.any(Billable));
     // });
 });
